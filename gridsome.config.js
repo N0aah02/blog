eee9a35a0806758e8222c868e442983ab33809ab
fabian9799 <fabian.cajochen29@gmail.com>
2020-11-08 16:47:27 +0100

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Netlify CMS Gridsome starter template',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.',
+  siteName: 'Fabian9799 Blog',
+  siteDescription: 'Tipps und so weil ich zu faul bin jeder Person das einzeln zu erklären',
 
   templates: {
     Post: '/:title',
